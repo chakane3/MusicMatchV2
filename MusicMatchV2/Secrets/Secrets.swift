df75fca6da78243f559598aaf5bef6eb6d75487b
chakane <39193557+chakane3@users.noreply.github.com>
2022-03-28 12:50:16 -0400

    added secrets 😈

---
+++ MusicMatchV2/Secrets/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  MusicMatchV2
+//
+//  Created by Chakane Shegog on 3/28/22.
+//
+
+import Foundation
+
+struct SecretAPIKey {
+    static let secreyKey = "2fbda91b968f88ccfdef5cc773dcc57d"
+}
