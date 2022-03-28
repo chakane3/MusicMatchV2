44147b2d51d9173e930bd3eecfd600563a697445
chakane <39193557+chakane3@users.noreply.github.com>
2022-03-28 16:22:21 -0400

    setup UI for artist->album->track->lyrics search

---
+++ MusicMatchV2/View Controllers/HomeView.swift
@@ -0,0 +1,29 @@
+//
+//  HomeView.swift
+//  MusicMatchV2
+//
+//  Created by Chakane Shegog on 3/28/22.
+//
+
+import UIKit
+
+class HomeView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
