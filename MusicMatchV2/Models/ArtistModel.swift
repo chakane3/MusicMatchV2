d57371189b56e3abe88c396ee0f1670f18098795
chakane <39193557+chakane3@users.noreply.github.com>
2022-03-28 12:44:31 -0400

    added secrets 😈

---
+++ MusicMatchV2/Models/ArtistModel.swift
@@ -0,0 +1,31 @@
+//
+//  ArtistModel.swift
+//  MusicMatchV2
+//
+//  Created by Chakane Shegog on 3/28/22.
+//
+
+import Foundation
+
+struct Artists: Codable {
+    let message: ArtistMessageBody
+}
+
+struct ArtistMessageBody: Codable {
+    let body: ArtistList
+}
+
+struct ArtistList: Codable {
+    let artist_list: [Artist]
+}
+
+struct Artist: Codable {
+    let artist: ArtistInfo
+}
+
+struct ArtistInfo: Codable {
+    let artist_id: Int
+    let artist_name: String
+    let artist_country: String
+    let artist_rating: Int
+}
