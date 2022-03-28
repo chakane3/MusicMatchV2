bcd7348e08b3ae83cd58e8acaaaa24a772604dfd
chakane <39193557+chakane3@users.noreply.github.com>
2022-03-28 13:32:57 -0400

    fetch artists API

--- MusicMatchV2/URLSession/MusixMatchAPIClient.swift
+++ MusicMatchV2/URLSession/MusixMatchAPIClient.swift
@@ -8,7 +8,29 @@
 import Foundation
 
 struct MusixMatchAPIClient {
-    static func fetchArtist(for artistName: String, completion: @escaping (Result<[Artists], NetworkError>) -> ()) {
+    static func fetchArtist(for artistName: String, completion: @escaping (Result<[Artist], NetworkError>) -> ()) {
+        // create a URLRequest object from our endpoint
+        let endpointURLString = "http://api.musixmatch.com/ws/1.1/artist.search?q_artist=\(artistName)&page_size=15&apikey=\(Secrets.apiKey)"
+        guard let url = URL(string: endpointURLString) else {
+            completion(.failure(.badURL(endpointURLString)))
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        // perform our data task
+        NetworkRequest.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let networkError):
+                completion(.failure(.networkClientError(networkError)))
+            case .success(let data):
+                do {
+                    let artists = try JSONDecoder().decode(Artists.self, from: data)
+                    completion(.success(artists.message.body.artist_list))
+                } catch {
+                    
+                }
+            }
+        }
         
     }
 }
